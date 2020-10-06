b21220bf409f0001a3ed20336fae589cf6c9557b
leo <waha1206@gmail.com>
2020-10-06 20:12:05 +0800

    mongodb-ok

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://waha1206:2lixgguu@cluster0.z6ie9.mongodb.net/node-server?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+// mongo DB
+const db = require('./config/keys').mongoURI
+
+// connect mongodb
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connect mongoDB'))
+  .catch((err) => {
+    console.log(err)
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`)
