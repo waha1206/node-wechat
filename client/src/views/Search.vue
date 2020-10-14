25da99ded51fe1182edcd08b7becfe64ae578238
leo <waha1206@gmail.com>
2020-10-14 19:26:58 +0800

    search 重複去除

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="circle">
     <Header title="商品搜尋" btn_icon="camera" :isLeft="true"></Header>
-    <div class="container">
-      <div class="search">
-        <input @keyup="inputSearch" v-model="getMessage" type="text" />
+    <!-- 內聯定義 class -->
+    <div class="container" :class="{ ac: classA, 'a-b': classB }">
+      <!-- 定義 class 在 data 裡面 -->
+      <div class="search" :class="classObj">
+        <input v-model="getMessage" type="text" />
         <p>{{ message }}</p>
       </div>
     </div>
@@ -20,21 +22,37 @@ export default {
   name: 'search',
   data() {
     return {
+      lastMessage: '',
       message: '',
-      momentsList: []
+      momentsList: [],
+      classA: true,
+      classB: false,
+      classObj: {
+        waha1205: false,
+        waha1206: true,
+        waha1207: true
+      }
+    }
+  },
+  watch: {
+    // 監聽 message 是否有改變，有改變就呼叫 this.DB() -- 此函數被設定 1 秒最多被觸發一次
+    message: function(newMessage, oldMessage) {
+      console.log('watch觸發次數')
+      this.DB()
     }
   },
   methods: {
     // 避免過於頻繁的往 server 端發送請求
     axiosSearchData() {
       // 如果有中文字，就發出 axios 請求
-      if (/.*[\u4e00-\u9fa5]+.*$/.test(this.message)) {
+      // 另外要增加，如果資料是一樣的，就不要重複輸出到後台
+      if (
+        /.*[\u4e00-\u9fa5]+.*$/.test(this.message) &&
+        this.message != this.lastMessage
+      ) {
         console.log(this.message)
+        this.lastMessage = this.message
       }
-    },
-    inputSearch() {
-      console.log('input端觸發次數')
-      this.DB()
     },
     // 測試用的
     btnClick() {
