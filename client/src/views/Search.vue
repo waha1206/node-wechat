42faf6d67ec0909d077d8708b60f76425ae4aaa0
waha1206 <waha1206@gmail.com>
2020-10-15 02:47:11 +0800

    要準備做todo這個經典例子

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -3,10 +3,14 @@
     <Header title="商品搜尋" btn_icon="camera" :isLeft="true"></Header>
     <!-- 內聯定義 class -->
     <div class="container" :class="{ ac: classA, 'a-b': classB }">
+      <div class="title">
+        <h1 v-if="user.name == 'waha1206'">主人你好</h1>
+        <h1 v-else>滾吧，你不是我主人</h1>
+      </div>
       <!-- 定義 class 在 data 裡面 -->
       <div class="search" :class="classObj">
         <input v-model="getMessage" type="text" />
-        <p>{{ message }}</p>
+        <p :style="PStyle">{{ message }}</p>
       </div>
     </div>
   </div>
@@ -31,6 +35,9 @@ export default {
         waha1205: false,
         waha1206: true,
         waha1207: true
+      },
+      PStyle: {
+        color: 'red'
       }
     }
   },
@@ -46,12 +53,25 @@ export default {
     axiosSearchData() {
       // 如果有中文字，就發出 axios 請求
       // 另外要增加，如果資料是一樣的，就不要重複輸出到後台
+      if (this.message.indexOf(' ') != -1) {
+        alert('搜尋字串不允許有空格')
+        this.message = ''
+      }
+      // 確定輸入是中文，並且無重複上次的搜尋資料
       if (
         /.*[\u4e00-\u9fa5]+.*$/.test(this.message) &&
         this.message != this.lastMessage
       ) {
         console.log(this.message)
         this.lastMessage = this.message
+        // 替換數組
+        // 如果資料來源已經在 vue data 裡面 可以用下面方式做過濾資料
+        //  example1.items = example1.items.filter(function (item) {
+        // return item.message.match(/Foo/)
+        // })
+        // https://cn.vuejs.org/v2/guide/list.html
+        // 显示过滤/排序后的结果
+        //有时，我们想要显示一个数组经过过滤或排序后的版本，而不实际变更或重置原始数据。在这种情况下，可以创建一个计算属性，来返回过滤或排序后的数组。
       }
     },
     // 測試用的
@@ -100,6 +120,11 @@ export default {
 </script>
 
 <style scoped>
+.title {
+  margin: 20px;
+  text-align: center;
+}
+
 .search {
   margin: 20px;
   text-align: center;
