d663bd06c1a197823c42e34d91e1e74d845bd168
leo <waha1206@gmail.com>
2020-10-21 19:29:34 +0800

    directives demo ok

--- client/src/components/CustomizedInput.vue
+++ client/src/components/CustomizedInput.vue
@@ -25,11 +25,26 @@ export default {
       }
     },
     demo: {
-      bind: {
-        function(el, binding, vnode) {
-          var s = JSON.stringify
-          el.innerHtml = 'name:' + s(binding.name) + '<br>'
-        }
+      bind: function(el, binding, vnode) {
+        var s = JSON.stringify
+        el.innerHTML =
+          'name: ' +
+          s(binding.name) +
+          '<br>' +
+          'value: ' +
+          s(binding.value) +
+          '<br>' +
+          'expression: ' +
+          s(binding.expression) +
+          '<br>' +
+          'argument: ' +
+          s(binding.arg) +
+          '<br>' +
+          'modifiers: ' +
+          s(binding.modifiers) +
+          '<br>' +
+          'vnode keys: ' +
+          Object.keys(vnode).join(', ')
       }
     }
   },
