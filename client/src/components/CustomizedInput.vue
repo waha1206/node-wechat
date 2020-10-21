5595f444f17371486fab740846bfdfece3127c05
leo <waha1206@gmail.com>
2020-10-21 19:21:24 +0800

    directives

--- client/src/components/CustomizedInput.vue
+++ client/src/components/CustomizedInput.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="customized-input">
     <input
+      v-focus
       type="text"
       :value="value"
       @input="$emit('input', $event.target.value)"
     />
+    <div id="hook-arguments-example" v-demo:foo.a.b="message"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'customized-input',
+  data() {
+    return {
+      message: 'hello'
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus()
+      }
+    },
+    demo: {
+      bind: {
+        function(el, binding, vnode) {
+          var s = JSON.stringify
+          el.innerHtml = 'name:' + s(binding.name) + '<br>'
+        }
+      }
+    }
+  },
   props: ['value']
 }
 </script>
