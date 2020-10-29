dcc5997e8831cb5d079dd3f3786e596df9aaf3c4
waha1206 <waha1206@gmail.com>
2020-10-30 02:23:14 +0800

    修改401過期沒登出的問題

--- client/src/http.js
+++ client/src/http.js
@@ -1,40 +1,40 @@
-import axios from 'axios';
-import router from './router';
+import axios from 'axios'
+import router from './router'
 
 // 請求攔截 request 再把 請求發送到 server 端之前，攔截後改變此 請求的內容
 axios.interceptors.request.use(
-  (config) => {
+  config => {
     if (localStorage.chatToken) {
-      config.headers.Authorization = localStorage.chatToken;
+      config.headers.Authorization = localStorage.chatToken
     }
-    return config;
+    return config
   },
-  (error) => {
-    return Promise.reject(error);
+  error => {
+    return Promise.reject(error)
   }
-);
+)
 
 // 響應攔截 response
 
 axios.interceptors.response.use(
-  (response) => {
-    return response;
+  response => {
+    return response
   },
 
-  (error) => {
+  error => {
     // 錯誤提醒
-    const { status } = erroe.response;
+    const { status } = error.response
     if (status == 401) {
-      alert('token 過期了，請重新登入！');
+      alert('token 過期了，請重新登入！')
       // 清除 token
-      localStorage.removeItem('chatToken');
+      localStorage.removeItem('chatToken')
       // 頁面跳轉
-      router.push('/login');
+      router.push('/login')
     } else {
-      alert(error.response.data);
+      alert(error.response.data)
     }
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default axios;
+export default axios
